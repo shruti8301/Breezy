1c9fe8cf59b2681b71408d40f92551a62e7981d5
Sai Sukesh <sukesh.sai04@gmail.com>
2021-05-27 16:26:37 +0530

    Main Question logic and code

--- app/src/main/java/com/example/breezy/QuestionActivity.java
+++ app/src/main/java/com/example/breezy/QuestionActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.RadioGroup;
@@ -14,30 +15,34 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import static com.example.breezy.helper.Questionnaire.getQuestions;
+
 public class QuestionActivity extends AppCompatActivity {
 
-    @BindView(R.id.question_view)
-    TextView question_view;
-    @BindView(R.id.question_number)
-    TextView question_number;
-    @BindView(R.id.question_progress)
-    ProgressBar question_progress;
-    @BindView(R.id.options_group)
-    RadioGroup options_group;
-    @BindView(R.id.previous_ques_btn)
-    TextView prev_btn;
-    @BindView(R.id.next_ques_btn)
-    Button next_btn;
+    @BindView(R.id.question_view) TextView question_view;
+    @BindView(R.id.question_number) TextView question_number;
+    @BindView(R.id.temp_type) TextView temp_type;
+    @BindView(R.id.question_progress) ProgressBar question_progress;
+    @BindView(R.id.options_group) RadioGroup options_group;
+    @BindView(R.id.previous_ques_btn) TextView prev_btn;
+    @BindView(R.id.next_ques) Button next_btn;
+    @BindView(R.id.next_ques_btn) Button next_ques_btn;
 
     private int position;
+    private List<String> currentCode;
     private JSONArray basicQuestions = null;
     private JSONObject currentQuestion;
+    private List<String> allQuestions, temp;
+    private Map<String, Integer> numberOfQues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +51,8 @@ public class QuestionActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        numberOfQues = new HashMap<>();
+        currentCode = new ArrayList<>();
         Map<String, Integer> basicQuesPoints = new HashMap<>();
         basicQuesPoints.put("Schizophrenia", 0);
         basicQuesPoints.put("PTSD", 0);
@@ -74,12 +81,34 @@ public class QuestionActivity extends AppCompatActivity {
         next_btn.setOnClickListener(view -> {
             try {
                 String code = currentQuestion.get("code").toString();
-                if (findViewById(options_group.getCheckedRadioButtonId()).getTag().toString().equals("yes"))
+                if (findViewById(options_group.getCheckedRadioButtonId()).getTag().toString().equals("yes")) {
                     basicQuesPoints.put(code, basicQuesPoints.get(code) + 1);
+                    numberOfQues.put(code, (Integer) currentQuestion.get("id"));
+                }
 
                 if (position == 4) {
-                    startActivity(new Intent(QuestionActivity.this, SplashActivity.class));
-                    finish();
+                    next_btn.setVisibility(View.GONE);
+                    next_ques_btn.setVisibility(View.VISIBLE);
+                    temp = new ArrayList<>();
+                    for (Map.Entry<String, Integer> check : basicQuesPoints.entrySet()) {
+                        if (check.getValue() == 1) {
+                            temp.add(check.getKey());
+                        }
+                    }
+                    allQuestions = getQuestions(temp);
+                    question_view.setText(allQuestions.get(0));
+                    position = 0;
+                    for (Map.Entry<String, Integer> check : numberOfQues.entrySet()) {
+                        for (int i = 0; i < check.getValue(); i++) {
+                            currentCode.add(check.getKey());
+                        }
+                    }
+                    temp_type.setText(currentCode.get(position));
+
+                    question_number.setText("Question " + "6/" + (allQuestions.size() + 5));
+                    question_progress.setMax(allQuestions.size() + 5);
+                    question_progress.setProgress(6);
+                    return;
                 }
                 currentQuestion = new JSONObject(basicQuestions.get(++position).toString());
                 question_view.setText(currentQuestion.get("ques").toString());
@@ -90,6 +119,22 @@ public class QuestionActivity extends AppCompatActivity {
             question_progress.setProgress(position + 1);
 
             Log.e("Info", basicQuesPoints.toString());
+        });
+
+        next_ques_btn.setOnClickListener(view -> {
+            if (findViewById(options_group.getCheckedRadioButtonId()).getTag().toString().equals("yes")) {
+                basicQuesPoints.put(currentCode.get(position), basicQuesPoints.get(currentCode.get(position)) + 1);
+                Log.e("Info", basicQuesPoints.toString());
+            }
+            if (position == allQuestions.size() - 1) {
+                startActivity(new Intent(QuestionActivity.this, SplashActivity.class));
+                finish();
+                return;
+            }
+            question_view.setText(allQuestions.get(++position));
+            question_number.setText("Question " + (position + 6) + "/" + (allQuestions.size() + 5));
+            question_progress.setProgress(position + 6);
+            temp_type.setText(currentCode.get(position));
         });
     }
 }
---
+++ app/src/main/java/com/example/breezy/helper/Questionnaire.java
@@ -0,0 +1,55 @@
+package com.example.breezy.helper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Questionnaire {
+
+    static String mainJsonStr = "{\n" +
+            "\"Schizophrenia\":[\"Do you struggle to trust that what you are thinking is real?\",\n" +
+            "\"Do you get the sense that others are controlling your thoughts and emotions?\",\n" +
+            "\"Do you struggle to keep up with daily living tasks such as showering, changing clothes, paying bills, cleaning, cooking, etc.?\",\n" +
+            "\"Do other people say that it is difficult for you to stay on subject or for them to understand you?\",\n" +
+            "\"Do you feel that you have little in common with family and friends?\",\n" +
+            "\"Do you feel that you are being tracked, followed, or watched at home or outside?\"],\n" +
+            "\"PTSD\":[\"Have you had nightmares or thought about the events when you did not want to?\",\n" +
+            "\"Have you tried hard not to think about the event(s) or went out of your way to avoid situations that reminded you of the event(s)?\",\n" +
+            "\"Have you been constantly on guard, watchful or easily startled?\",\n" +
+            "\"Have you felt numb or detached from people, activities, or your surroundings?\",\n" +
+            "\"Have you felt guilty or unable to stop blaming yourself or others for the event(s) or any problems the event(s) may have caused?\"],\n" +
+            "\"OCD\":[\"Do you have frequent unwanted thoughts that seem uncontrollable?\",\n" +
+            "\"Do you keep many useless things because you feel that you can’t throw them away?\",\n" +
+            "\"Do you have rituals or repetitive behaviours that take a lot of time in a day?\",\n" +
+            "\"Do you keep checking things over and over again?\",\n" +
+            "\"Do your daily activities take a long time to complete?\"],\n" +
+            "\"Depression\":[\"Have you or your family been into troubles lately?\",\n" +
+            "\"Poor appetite or overeating?\",\n" +
+            "\"Do you feel hopeless or unhappy frequently?\",\n" +
+            "\"Do you face difficulty in making decisions?\",\n" +
+            "\"Does it take great effort for you to do simple things?\"],\n" +
+            "\"Anxiety\":[\"Being so restless that it is hard to sit still?\",\n" +
+            "\"Feeling afraid, as if something awful might happen\",\n" +
+            "\"Worrying too much about different things\",\n" +
+            "\"Does worry or anxiety interfere with falling and/or staying asleep?\",\n" +
+            "\"Do you experience strong fear that causes panic, shortness of breath, chest pains, a pounding heart, sweating, shaking, nausea, dizziness and/or fear of dying?\"]\n" +
+            "}";
+
+    public static JSONObject mainJson;
+
+    public static List<String> getQuestions(List<String> disease) throws JSONException {
+        List<String> questions = new ArrayList<>();
+
+        if (mainJson == null){
+            mainJson = new JSONObject(mainJsonStr);
+        }
+        for (String i : disease){
+            for (int j = 0; j < mainJson.getJSONArray(i).length(); j++) {
+                questions.add(mainJson.getJSONArray(i).get(j).toString());
+            }
+        }
+        return questions;
+    }
+}
